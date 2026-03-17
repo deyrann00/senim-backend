7d9211713ed1472e3ca437f166d586ff1137b897
d1rannn <denchik.stars06@gmail.com>
2026-03-17 18:40:02 +0500

    Init commit

---
+++ src/test/java/com/senim/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senim.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
